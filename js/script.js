c43c39c86f156e83297d2dae06d8183719264911
Vida <134993237+VidaWee@users.noreply.github.com>
2023-06-22 22:36:55 +1200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+var fans = document.querySelector(".fans");
+var fish = document.querySelector(".fish");
+var pets = document.querySelector(".pets");
+
+fans.addEventListener("mouseover" , function(){
+
+    fans.innerText = "123K";
+});
+
+fish.addEventListener("mouseover" , function(){
+
+    fish.innerText = "47K";
+});
+
+pets.addEventListener("mouseover" , function(){
+
+    pets.innerText = "20K";
+});
