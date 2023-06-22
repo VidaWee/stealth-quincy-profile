6db6550fc71b9b737f0da0cee68892e4af881eba
Vida <134993237+VidaWee@users.noreply.github.com>
2023-06-22 22:47:56 +1200

    switch toggle dark light

--- js/script.js
+++ js/script.js
@@ -16,3 +16,22 @@ pets.addEventListener("mouseover" , function(){
 
     pets.innerText = "20K";
 });
+
+var toggle = document.querySelector("#toggle");
+var ball = document.querySelector(".ball");
+var h1 = document.querySelector("h1");
+var body = document.querySelector("body");
+
+toggle.addEventListener("click", function(){
+
+    if (body.classList.contains("light")) {
+        body.classList.remove("light");
+        ball.classList.remove("move-right");
+        h1.innerText = "Stealth Quincy";
+      } else {
+        body.classList.add("light");
+        ball.classList.add("move-right");
+        h1.innerText = "Party Quincy";
+      }
+
+});
